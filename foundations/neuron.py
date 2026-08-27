f7661241a5b5819411aed93e8e98dcce7b2de750
Shashank Reddy Lankala <158861887+shashank1027@users.noreply.github.com>
2026-08-27 12:48:52 -0500

    Update Single neuron forward pass (Build a Neural Net 1/5)

--- foundations/neuron.py
+++ foundations/neuron.py
@@ -1,7 +1,5 @@
 import numpy as np
 from numpy.typing import NDArray
-
-
 class Solution:
     def forward(self, x: NDArray[np.float64], w: NDArray[np.float64], b: float, activation: str) -> float:
         z=np.dot(x,w)+b
